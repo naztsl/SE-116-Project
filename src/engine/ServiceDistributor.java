8954eeefa292de2517830fb0ab595abaedd35005
naztsl <tasalnaz@gmail.com>
2026-06-03 02:16:22 +0300

    added ServiceDistributor class

--- src/engine/ServiceDistributor.java
+++ src/engine/ServiceDistributor.java
@@ -6,13 +6,13 @@ public class ServiceDistributor {
 
     public void distributeServices(Cell[][] grid) {
 
-        for(int i = 0; i < grid.length; i++) {
+        for (int i = 0; i < grid.length; i++) {
 
-            for(int j = 0; j < grid[i].length; j++) {
+            for (int j = 0; j < grid[i].length; j++) {
 
                 Cell current = grid[i][j];
 
-                if(current instanceof ServiceBuilding) {
+                if (current instanceof ServiceBuilding) {
 
                     ServiceBuilding service =
                             (ServiceBuilding) current;
@@ -26,13 +26,13 @@ public class ServiceDistributor {
     private void applyServiceToZones(ServiceBuilding service,
                                      Cell[][] grid) {
 
-        for(int i = 0; i < grid.length; i++) {
+        for (int i = 0; i < grid.length; i++) {
 
-            for(int j = 0; j < grid[i].length; j++) {
+            for (int j = 0; j < grid[i].length; j++) {
 
                 Cell current = grid[i][j];
 
-                if(current instanceof Zone) {
+                if (current instanceof Zone) {
 
                     Zone zone = (Zone) current;
 
